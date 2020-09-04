0997ad2522380dc9b3812037822401ac3503e501
serge <sergeuniquite@gmail.com>
2020-09-04 17:16:33 +0300

    saving

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 
 const app = express();
-const _DB_ = "mongodb+srv://admin:powermongo@cluster0-dbfzg.mongodb.net/test?retryWrites=true&w=majority";
+const _DB_ = `mongodb+srv://admin:${process.env.MONGO_BASE_PASSWORD}@cluster0-dbfzg.mongodb.net/test?retryWrites=true&w=majority`;
 const port = process.env.EXPRESS_PORT || 8080;
 
 let User = require('./models/user');
@@ -12,7 +12,6 @@ mongoose.connect(_DB_, {useNewUrlParser: true});
 
 const db = mongoose.connection;
 
-console.log('User', User);
 
 
 db.on('error', error => console.log('error', error));
@@ -30,53 +29,13 @@ app.get('/roles/:role', (req, res) => {
     })
 })
 
+app.get('/set/:name', (req, res) => {
+    const newUser = new User({ name: req.params.name, role: 'user' });
+
+    newUser.save().then((user) => {
+        res.send(user);
+    });
+})
+
 
 app.listen(port, () => console.log(`working on ${port}`))
-
-// const User = mongoose.model('User', { name: String, role: String });
-
-
-// console.log('User', User);
-
-// console.log('user', user);
-
-// const Zildjian = new User({ name: 'Zildjian', role: 'user' });
-
-// Zildjian.save().then((x) => {
-//     console.log('x', x);
-// });
-
-
-/* mongo> */
-/* const MongoClient = require('mongodb').MongoClient;
-
-console.log('MongoClient', MongoClient);
-const client = new MongoClient(_DB_, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-
-  console.log('collection', collection);
-  client.close();
-}); */
-/* <mongo */
-
-
-/* plain app> */
-/* app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
-app.get('/api/hello', (req, res) => {
-    res.send([{text: 'Hello World'}]);
-})
-
-app.get('/api/test', (req, res) => {
-    res.send([{test: `Test`}]);
-})
-
-app.get('/api/params/:id/:salt', (req, res) => {
-    res.send({params: req.params, query: req.query});
-}) */
-/* <plain app */
-
-// console.log('app', app);
