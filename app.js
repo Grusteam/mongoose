ebec292f0d70015f0147cd4d850a9b3fa4e761a9
Serge <sergeuniquite@gmail.com>
2020-09-07 22:58:31 +0300

    1

--- app.js
+++ app.js
@@ -39,49 +39,11 @@ db.once('open', () => console.log('db connected'));
 /*  */
 app.listen(port, () => console.log(`working on ${port}`))
 
-const showResults = all => {
-    const filtered = all.filter(({ tag }) => tag !== 'favicon.ico')
-    const sorted = filtered.sort((a, b) => b.count - a.count);
-    const arr = filtered.map(({ tag, count }) => {
-        return `<tr class="tr"><td class="td">${tag}</td><td class="td">${count}</td></tr>`
-    });
-    const str = arr.join('');
-    const table = `<table class="table">${str}</table>`;
-    const result = indexLayout.replace('{{replace}}', table);
-
-    return result;
-}
-
-/*  */
-app.get('/:tag/:any?', async (req, res) => {
-    const { tag } = req.params;
-
-    /* show */
-    if (tag === 'show_results') {
-        const all = await Record.find();
-        const layout = showResults(all);
-
-        return res.send(layout);
-    }
-
-    const filter = {tag};
-    const searchResult = await Record.findOne(filter);
-
-    /* update */
-    if (searchResult) {
-        searchResult.count++;
-
-        const updated = await searchResult.save();
-    /* new */
-    } else {
-        const newRecord = new Record({ tag, count: 1 });
-        const saved = await newRecord.save();
-    }
-
-    res.redirect(bigBeads);
-})
-
 /* root */
 app.get('/', (req, res) => {
-    res.redirect(bigBeads);
+    res.send('home');
+})
+
+app.get('/frontend', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
 })
---
+++ public/precache-manifest.74c338bf96a1eb2a0e9426012ab4cb98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb929d2354c502fe32633996cf24ef36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d43e4a9f9648e51e34f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d43e4a9f9648e51e34f",
+    "url": "/static/js/main.2f95536a.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
